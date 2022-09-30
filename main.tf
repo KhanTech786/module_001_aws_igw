c2d683a7ce6b58bcd2be499b6e7d48772b2f4b91
Khan.A <aiymankhan786@gmail.com>
2022-09-30 12:13:19 -0500

    update dependency script

--- main.tf
+++ main.tf
@@ -1,3 +1,4 @@
 resource "aws_internet_gateway" "khans_igw" {
+  vpc_id = var.igw_vpc_id
   tags   = var.igw_tags
 }
--- variable_dec.tf
+++ variable_dec.tf
@@ -2,3 +2,8 @@ variable "igw_tags" {
   description = "IGW tags"
   type        = map(string)
 }
+
+variable "igw_vpc_id" {
+  description = "IGW VPC ID"
+  type = string
+}
